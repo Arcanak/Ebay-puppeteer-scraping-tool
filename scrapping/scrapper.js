ebf52583c36368891e80ef5122f1371d09ab3d96
Bryan <bafricano@ginzo.tech>
2022-10-04 11:32:04 +0200

    feat: changed script for faster filter application

--- scrapping/scrapper.js
+++ scrapping/scrapper.js
@@ -26,31 +26,34 @@ async function getCompletedOrders(page) {
     await page.waitForSelector('#gh-ac');
     await page.type('#gh-ac', search);
     await page.click('input[type="submit"]');
-    const checkboxes = 'body > div.srp-main.srp-main--isLarge > div.srp-rail__left > ul > li:nth-child(1) > ul > li';
-    await page.waitForSelector(checkboxes);
-    //await page.waitForSelector('body > div.srp-main.srp-main--isLarge > div.srp-rail__left > ul > li:nth-child(1) > ul > li:nth-child(7) > .x-refine__group > ul');
-    //const checkboxes = 'body > div.srp-main.srp-main--isLarge > div.srp-rail__left > ul > li:nth-child(1) > ul > li:nth-child(7) > .x-refine__group > ul';
-    const elements = await page.$$eval(checkboxes, (elements) => {
-       return elements.map(elem => elem.innerText);
-    });
-    await page.click('body > div.srp-main.srp-main--isLarge > div.srp-rail__left > ul > li:nth-child(1) > ul > li:nth-child(' + (elements.length)  + ') > .x-refine__group > ul > li:nth-child(5) > .x-refine__multi-select > a');
+    await page.waitForSelector('body');
+    const params = '&_sacat=0&LH_TitleDesc=0&LH_BIN=1&LH_Complete=1&LH_ItemCondition=1000%7C2500&LH_Sold=1'
+    await page.goto(page.url() + params);
+    // const checkboxes = 'body > div.srp-main.srp-main--isLarge > div.srp-rail__left > ul > li:nth-child(1) > ul > li';
+    // await page.waitForSelector(checkboxes);
+    // //await page.waitForSelector('body > div.srp-main.srp-main--isLarge > div.srp-rail__left > ul > li:nth-child(1) > ul > li:nth-child(7) > .x-refine__group > ul');
+    // //const checkboxes = 'body > div.srp-main.srp-main--isLarge > div.srp-rail__left > ul > li:nth-child(1) > ul > li:nth-child(7) > .x-refine__group > ul';
+    // const elements = await page.$$eval(checkboxes, (elements) => {
+    //    return elements.map(elem => elem.innerText);
+    // });
+    // await page.click('body > div.srp-main.srp-main--isLarge > div.srp-rail__left > ul > li:nth-child(1) > ul > li:nth-child(' + (elements.length)  + ') > .x-refine__group > ul > li:nth-child(5) > .x-refine__multi-select > a');
     
-    // Only buy it now
-    await page.waitForSelector('div.srp-controls__default-refinements.clearfix');
-    await page.click('div.srp-controls__default-refinements.clearfix > div.srp-controls--responsive.srp-controls__row-cells.left > div.srp-controls__control--responsive > div > ul > li:nth-child(4) > a');
-    // New and used
-    await page.waitForSelector('body > div.srp-main.srp-main--isLarge > div.srp-rail__left > ul > li:nth-child(1) > ul > li:nth-child(3) > .x-refine__group > ul');
-    await page.click('body > div.srp-main.srp-main--isLarge > div.srp-rail__left > ul > li:nth-child(1) > ul > li:nth-child(3) > .x-refine__group > ul > li:nth-child(1) > .x-refine__multi-select > a');
-    await page.waitForSelector('body > div.srp-main.srp-main--isLarge > div.srp-rail__left > ul > li:nth-child(1) > ul > li:nth-child(3) > .x-refine__group > ul');
-    await page.click('body > div.srp-main.srp-main--isLarge > div.srp-rail__left > ul > li:nth-child(1) > ul > li:nth-child(3) > .x-refine__group > ul > li:nth-child(4) > .x-refine__multi-select > a');
-    // Info about items
-    await page.waitForSelector('#srp-river-results > ul');
-    // build item object
-    const items = await page.$$eval('#srp-river-results > ul > li.s-item', (elements) => {
-        return elements.map(elem => elem.children);
-    }).catch(err => {
-        console.log(err);
-    });
-    console.log(items);
+    // // Only buy it now
+    // await page.waitForSelector('div.srp-controls__default-refinements.clearfix');
+    // await page.click('div.srp-controls__default-refinements.clearfix > div.srp-controls--responsive.srp-controls__row-cells.left > div.srp-controls__control--responsive > div > ul > li:nth-child(4) > a');
+    // // New and used
+    // await page.waitForSelector('body > div.srp-main.srp-main--isLarge > div.srp-rail__left > ul > li:nth-child(1) > ul > li:nth-child(3) > .x-refine__group > ul');
+    // await page.click('body > div.srp-main.srp-main--isLarge > div.srp-rail__left > ul > li:nth-child(1) > ul > li:nth-child(3) > .x-refine__group > ul > li:nth-child(1) > .x-refine__multi-select > a');
+    // await page.waitForSelector('body > div.srp-main.srp-main--isLarge > div.srp-rail__left > ul > li:nth-child(1) > ul > li:nth-child(3) > .x-refine__group > ul');
+    // await page.click('body > div.srp-main.srp-main--isLarge > div.srp-rail__left > ul > li:nth-child(1) > ul > li:nth-child(3) > .x-refine__group > ul > li:nth-child(4) > .x-refine__multi-select > a');
+    // // Info about items
+    // await page.waitForSelector('#srp-river-results > ul');
+    // // build item object
+    // const items = await page.$$eval('#srp-river-results > ul > li.s-item', (elements) => {
+    //     return elements.map(elem => elem);
+    // }).catch(err => {
+    //     console.log(err);
+    // });
+    // console.log(items);
 
 }
