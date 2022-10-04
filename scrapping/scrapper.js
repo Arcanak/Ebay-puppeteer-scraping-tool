29b296146bafd13535725e48ac9810094d37d5ce
Bryan <bafricano@ginzo.tech>
2022-10-04 13:21:06 +0200

    feat: now the script gets data from all pages

--- scrapping/scrapper.js
+++ scrapping/scrapper.js
@@ -27,8 +27,30 @@ async function getCompletedOrders(page) {
     await page.type('#gh-ac', search);
     await page.click('input[type="submit"]');
     await page.waitForSelector('body');
-    const params = '&_sacat=0&LH_TitleDesc=0&LH_BIN=1&LH_Complete=1&LH_ItemCondition=1000%7C2500&LH_Sold=1'
+    const params = '&_sacat=0&LH_TitleDesc=0&LH_BIN=1&LH_Complete=1&LH_ItemCondition=1000%7C2500&LH_Sold=1&_ipg=240'
     await page.goto(page.url() + params);
+    await page.waitForSelector('body');
+    const pages = await page.$$eval('#srp-river-results > ul > li.srp-river-answer.srp-river-answer--BASIC_PAGINATION_V2.undefined > div.s-pagination > span > span > nav > ol > li', (el) => el.length);
+    var itemInfo = [];
+    for (let i = 1; i <= pages; i++) {
+        var itemPageData = (await page.$$eval('li.s-item', (items) => {
+            var itemPageData = [];
+            items.forEach((item) => {
+                const priceText = item.querySelector('.s-item__price').innerText;
+                const price = parseFloat(priceText.replace(/[^0-9.-]+/g,""));
+                const status = item.querySelector('.SECONDARY_INFO').innerText;
+                const date = item.querySelector('.s-item__info.clearfix > div > div > span').innerHTML.slice(6);
+                const title = item.querySelector('.s-item__title').innerText;
+                itemPageData.push({price, status, title, date});
+            });
+            return itemPageData;            
+        }).catch((err) => {
+            console.log(err);
+        }));
+        itemInfo = itemInfo.concat(itemPageData);
+        await page.goto(page.url() + '&_pgn=' + (i + 1));
+    }
+    console.log(JSON.stringify(itemInfo));
     // const checkboxes = 'body > div.srp-main.srp-main--isLarge > div.srp-rail__left > ul > li:nth-child(1) > ul > li';
     // await page.waitForSelector(checkboxes);
     // //await page.waitForSelector('body > div.srp-main.srp-main--isLarge > div.srp-rail__left > ul > li:nth-child(1) > ul > li:nth-child(7) > .x-refine__group > ul');
